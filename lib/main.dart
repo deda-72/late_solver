6331302d1ec7dc2d598044eecf33d4b087f46709
Tim Kazaev <Timofeykazaev@gmail.com>
2024-06-28 17:12:24 -0500

    updating letters

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,123 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       // home: const MyHomePage(title: 'Flutter Demo Home Page test'),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Row of Labels'),
+//         ),
+//         body: Column(
+//           children: [
+//             Expanded(
+//               child: Center(
+//                 child: RowOfLabels(),
+//               ),
+//             ),
+//             Padding(
+//               padding: EdgeInsets.only(bottom: 35.0),
+//               child: CustomKeyboard(
+//                 onKeyPressed: (String key) {
+//                   // Handle key press
+//                   print('Key pressed: $key');
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class RowOfLabels extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: List.generate(5, (index) {
+//         return Container(
+//           width: 60.0,
+//           height: 60.0,
+//           margin: EdgeInsets.all(5.0),
+//           decoration: BoxDecoration(
+//             border: Border.all(color: Colors.black),
+//             color: Colors.white,
+//           ),
+//           child: Center(
+//             child: Text(
+//               'L${index + 1}',
+//               style: TextStyle(fontSize: 18.0),
+//             ),
+//           ),
+//         );
+//       }),
+//     );
+//   }
+// }
+
+// class CustomKeyboard extends StatelessWidget {
+//   final Function(String) onKeyPressed;
+
+//   CustomKeyboard({required this.onKeyPressed});
+
+//   final List<List<String>> keys = [
+//     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+//     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+//     ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Back'],
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisSize: MainAxisSize.min,
+//       children: keys.map((row) {
+//         return Padding(
+//           padding: const EdgeInsets.symmetric(vertical: 4.0),
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: row.map((key) {
+//               // Adjust width for Enter and Back keys
+//               double width = (key == 'Enter' || key == 'Back') ? 50.0 : 30.0;
+//               return Padding(
+//                 padding: const EdgeInsets.symmetric(horizontal: 4.0),
+//                 child: SizedBox(
+//                   width: width,
+//                   height: 40,
+//                   child: ElevatedButton(
+//                     onPressed: () => onKeyPressed(key),
+//                     child: Text(key),
+//                     style: ElevatedButton.styleFrom(
+//                       padding: EdgeInsets.zero,
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius:
+//                             BorderRadius.circular(8.0), // Adjust radius here
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               );
+//             }).toList(),
+//           ),
+//         );
+//       }).toList(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,59 +127,79 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       // home: const MyHomePage(title: 'Flutter Demo Home Page test'),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Row of Labels'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: RowOfLabels(),
-              ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // List to hold the selected letters
+  List<String> selectedLetters = ['', '', '', '', ''];
+
+  // Index to track the current label position
+  int currentLabelIndex = 0;
+
+  void handleKeyPressed(String key) {
+    if (currentLabelIndex < 5) {
+      setState(() {
+        selectedLetters[currentLabelIndex] = key;
+        currentLabelIndex++;
+      });
+    }
+  }
+
+  void handleReset() {
+    setState(() {
+      selectedLetters = ['', '', '', '', ''];
+      currentLabelIndex = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Row of Labels'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: RowOfLabels(selectedLetters: selectedLetters),
             ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 35.0),
-              child: CustomKeyboard(
-                onKeyPressed: (String key) {
-                  // Handle key press
-                  print('Key pressed: $key');
-                },
-              ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 35.0),
+            child: CustomKeyboard(
+              onKeyPressed: handleKeyPressed,
+              onResetPressed: handleReset,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
 
 class RowOfLabels extends StatelessWidget {
+  final List<String> selectedLetters;
+
+  RowOfLabels({required this.selectedLetters});
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -75,7 +215,9 @@ class RowOfLabels extends StatelessWidget {
           ),
           child: Center(
             child: Text(
-              'L${index + 1}',
+              selectedLetters[index].isEmpty
+                  ? 'L${index + 1}'
+                  : selectedLetters[index],
               style: TextStyle(fontSize: 18.0),
             ),
           ),
@@ -87,8 +229,9 @@ class RowOfLabels extends StatelessWidget {
 
 class CustomKeyboard extends StatelessWidget {
   final Function(String) onKeyPressed;
+  final VoidCallback onResetPressed;
 
-  CustomKeyboard({required this.onKeyPressed});
+  CustomKeyboard({required this.onKeyPressed, required this.onResetPressed});
 
   final List<List<String>> keys = [
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
@@ -114,7 +257,13 @@ class CustomKeyboard extends StatelessWidget {
                   width: width,
                   height: 40,
                   child: ElevatedButton(
-                    onPressed: () => onKeyPressed(key),
+                    onPressed: () {
+                      if (key == 'Back') {
+                        onResetPressed();
+                      } else {
+                        onKeyPressed(key);
+                      }
+                    },
                     child: Text(key),
                     style: ElevatedButton.styleFrom(
                       padding: EdgeInsets.zero,
@@ -133,92 +282,3 @@ class CustomKeyboard extends StatelessWidget {
     );
   }
 }
-
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
